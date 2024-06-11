d6092277c19862c03199640d45ecb8e588ba2524
unknown <lateralcreativity@outlook.com>
2024-06-11 14:19:32 -0500

    challenge 1

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,15 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        // print your initials
+        System.out.println("""
+                 GGG      DDDD
+                G   G     D   D
+                G         D   D
+                G GGG     D   D
+                G   G     D   D
+                G   G     D   D
+                 GGG      DDDD
+                """);
     }
 }
