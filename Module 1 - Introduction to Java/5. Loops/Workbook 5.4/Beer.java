4ec00650e7a4f16743c91364255f142d4a6b25c1
unknown <lateralcreativity@outlook.com>
2024-06-12 12:31:37 -0500

    5.4 workbook

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
@@ -1,6 +1,15 @@
 public class Beer {
     public static void main(String[] args) {
        // See detailed instructions on Learn the Part.
+        for (int i = 99; i > 0; i--) {
+            sing(i);
+        }
     }
 
+    public static void sing(int i) {
+        System.out.printf("""
+                %d bottles of beer on the wall, %d bottles of beer!
+                take one down, pass it around, %d bottles of beer on the wall!%n
+                """, i, i, i-1);
+    }
 }
