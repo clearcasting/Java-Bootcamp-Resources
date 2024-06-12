4967a51151965b3024ec02205e1145c6e7f2346f
unknown <lateralcreativity@outlook.com>
2024-06-12 11:11:30 -0500

    5.3 done

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -5,6 +5,14 @@ public class CountingTool {
         Scanner scan = new Scanner(System.in);
 
         // See Learn the Part for the instructions.
+        System.out.print("Hi Timmy! Choose a number to count to: ");
+        int number = scan.nextInt();
+
+        for (int i = 0; i < number + 1; i++) {
+            System.out.print(i + " ");
+        }
+
+
 
         scan.close();
     }
