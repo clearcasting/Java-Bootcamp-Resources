6a59c8e2f587852a1dd4988121081f8dd01f01a9
unknown <lateralcreativity@outlook.com>
2024-06-12 13:29:58 -0500

    workbook 5.7

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
@@ -1,8 +1,20 @@
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        // See Learn the Part for detailed instructions.
+//        A number is a multiple of X if number ÷ X results in a remainder of zero.
+//        There are three conditions for Fizz, Buzz, and FizzBuzz. Be careful about their order.
+//        If something is FizzBuzz, it also satisfies the conditions for Fizz and Buzz.
 
+        // See Learn the Part for detailed instructions.
+        for (int i = 0; i < 19; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + " FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println(i + " Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println(i + " Fizz");
+            }
+        }
     }
 
 }
