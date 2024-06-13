ae12439177e816e09fa92e39a2abcf42a1b06e57
clearcasting <lateralcreativity@outlook.com>
2024-06-13 14:31:13 -0500

    6.13 revisit done

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
@@ -29,10 +29,22 @@ public class Prices {
                 {8.99, 7.99, 9.49, 9.99, 10.99}
         };
 
+        for (int i = 0; i < prices.length; i++) {
+            switch (i) {
+                case 0: System.out.print("Baking: "); break;
+                case 1: System.out.print("Beverage: "); break;
+                case 2: System.out.print("Cereals: "); break;
+            }
+            for (int j = 0; j < prices[i].length; j++) {
+                System.out.print(prices[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
 
-        System.out.println("Baking: " + prices[0][0] + " " + prices[0][1] + " " + prices[0][2] + " " + prices[0][3] + " " + prices[0][4]);
-        System.out.println("Beverage: " + prices[1][0] + " " + prices[1][1] + " " + prices[1][2] + " " + prices[1][3] + " " + prices[1][4]);
-        System.out.println("Cereals: " + prices[2][0] + " " + prices[2][1] + " " + prices[2][2] + " " + prices[2][3] + " " + prices[2][4]);
+
+//        System.out.println("Baking: " + prices[0][0] + " " + prices[0][1] + " " + prices[0][2] + " " + prices[0][3] + " " + prices[0][4]);
+//        System.out.println("Beverage: " + prices[1][0] + " " + prices[1][1] + " " + prices[1][2] + " " + prices[1][3] + " " + prices[1][4]);
+//        System.out.println("Cereals: " + prices[2][0] + " " + prices[2][1] + " " + prices[2][2] + " " + prices[2][3] + " " + prices[2][4]);
 
 
     }
