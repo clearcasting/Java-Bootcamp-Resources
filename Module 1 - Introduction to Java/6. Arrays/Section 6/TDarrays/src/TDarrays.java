12b8d6e9beb76b022682b0f025731b2209342c23
clearcasting <lateralcreativity@outlook.com>
2024-06-13 14:07:35 -0500

    done

---
+++ Module 1 - Introduction to Java/6. Arrays/Section 6/TDarrays/src/TDarrays.java
@@ -0,0 +1,13 @@
+public class TDarrays {
+    public static void main(String[] args) {
+        int[][] grades = {
+                {72, 74, 78, 76},
+                {65, 64, 61, 67},
+                {95, 98, 99, 100}
+        };
+
+        System.out.println("\tHarry: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
+        System.out.println("\tRon: " + grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
+        System.out.println("\tHermione: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
+    }
+}
