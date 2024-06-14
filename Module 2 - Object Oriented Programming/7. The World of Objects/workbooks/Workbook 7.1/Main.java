01f1e57b46c8100e05f1c132f93eeecc60f44d4a
clearcasting <lateralcreativity@outlook.com>
2024-06-14 09:29:16 -0500

    done

--- Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
@@ -1,3 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "Johnny";
+        System.out.println(person.name);
+
+        person.nationality = "British";
+        System.out.println(person.nationality);
+
+        person.dateOfBirth = "01/01/2001";
+        System.out.println(person.dateOfBirth);
+
+        person.passport = new String[]{
+                person.name, person.nationality, person.dateOfBirth
+        };
+        System.out.println(Arrays.toString(person.passport));
+
+        person.seatNumber = 38;
+        System.out.println(person.seatNumber);
+    }
 }
