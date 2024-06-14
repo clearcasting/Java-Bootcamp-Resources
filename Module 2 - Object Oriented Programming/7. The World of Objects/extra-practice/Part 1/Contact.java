dd04d6a494f3b12d268e382a7159874dd8751fb5
clearcasting <lateralcreativity@outlook.com>
2024-06-14 15:30:43 -0500

    done

--- Module 2 - Object Oriented Programming/7. The World of Objects/extra-practice/Part 1/Contact.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/extra-practice/Part 1/Contact.java
@@ -51,4 +51,12 @@ public class Contact {
     public int getAge() {
         return age;
     }
+
+    public String toString() {
+        return "Name: " + this.name + "\n" +
+                "Phone number: " + this.phoneNumber + "\n" +
+                "Birth Date: " + this.birthDate + "\n" +
+                "Age: " + this.age + " year old\n";
+    }
+
 }
