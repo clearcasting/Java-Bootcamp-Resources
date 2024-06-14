b08f37336698bfc39f2b7ed38f51c762b91da050
clearcasting <lateralcreativity@outlook.com>
2024-06-14 14:29:53 -0500

    part 2 done

--- Module 2 - Object Oriented Programming/7. The World of Objects/extra-practice/Part 1/Contact.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/extra-practice/Part 1/Contact.java
@@ -1,14 +1,17 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Contact {
     private String name;
     private String phoneNumber;
     private String birthDate;
     private int age;
 
-    public Contact(String name, String phoneNumber, String birthDate, int age) {
+    public Contact(String name, String phoneNumber, String birthDate) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.birthDate = birthDate;
-        this.age = age;
+        this.age = toAge(birthDate);
     }
 
     public Contact(Contact source) {
@@ -16,5 +19,20 @@ public class Contact {
         this.phoneNumber = source.phoneNumber;
         this.birthDate = source.birthDate;
         this.age = source.age;
+    }
+
+    public int toAge(String birthDate) {
+        LocalDate now = LocalDate.now();
+        Period period = Period.between(LocalDate.parse(birthDate), now);
+        return period.getYears();
+    }
+
+    private void setAge() {
+        this.age = toAge(birthDate);
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+        setAge();
     }
 }
