6e2837dbef1124b9c9e4a3467c1b4a70ab1a116e
clearcasting <lateralcreativity@outlook.com>
2024-06-17 18:05:29 -0500

    part 1 done

--- Module 2 - Object Oriented Programming/19. Capstone/starter-project/pojo/CashAccount.java
+++ Module 2 - Object Oriented Programming/19. Capstone/starter-project/pojo/CashAccount.java
@@ -1,6 +1,25 @@
 package pojo;
 
-public class CashAccount {
+import java.math.BigDecimal;
 
+public class CashAccount extends TradeAccount {
+    private BigDecimal cashBalance;
 
+    public CashAccount(String id, BigDecimal cashBalance) {
+        super(id);
+        this.cashBalance = cashBalance;
+    }
+
+    public BigDecimal getCashBalance() {
+        return cashBalance;
+    }
+
+    public void setCashBalance(BigDecimal cashBalance) {
+        this.cashBalance = cashBalance;
+    }
+
+    @Override
+    public CashAccount clone() {
+        return new CashAccount(getId(), getCashBalance());
+    }
 }
