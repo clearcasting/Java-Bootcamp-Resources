3f693a58298bbb41adcf333996cf226ecfcc69f7
clearcasting <lateralcreativity@outlook.com>
2024-06-17 17:28:39 -0500

    InterfaceLesson

--- Module 2 - Object Oriented Programming/18. Interface/starter-project/service/CheckingService.java
+++ Module 2 - Object Oriented Programming/18. Interface/starter-project/service/CheckingService.java
@@ -1,8 +1,47 @@
 package service;
 
 
-public class CheckingService {
-        
+import pojo.Checking;
+import repository.AccountRepository;
 
+import java.math.BigDecimal;
+
+public class CheckingService implements AccountService {
+
+    AccountRepository accountRepository;
+
+    public CheckingService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public void createAccount(Checking checking) {
+        accountRepository.createAccount(checking);
+    }
+
+    public Checking retrieveChecking(String id) {
+        return (Checking) accountRepository.retrieveAccount(id);
+    }
+
+    public void updateChecking(Checking checking) {
+        accountRepository.updateAccount(checking);
+    }
+
+    public void deleteChecking(String id) {
+        accountRepository.deleteAccount(id);
+    }
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        Checking account = retrieveChecking(id);
+        account.setBalance(account.getBalance().add(amount));
+        updateChecking(account);
+    }
+
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        Checking account = retrieveChecking(id);
+        account.setBalance(account.getBalance().subtract(amount));
+        updateChecking(account);
+    }
 
 }
--- Module 2 - Object Oriented Programming/18. Interface/starter-project/service/CreditService.java
+++ Module 2 - Object Oriented Programming/18. Interface/starter-project/service/CreditService.java
@@ -1,8 +1,46 @@
 package service;
 
 
-public class CreditService {
+import pojo.Credit;
+import repository.AccountRepository;
 
+import java.math.BigDecimal;
 
+public class CreditService implements AccountService {
 
+    AccountRepository accountRepository;
+
+    public CreditService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public void createAccount(Credit credit) {
+        accountRepository.createAccount(credit);
+    }
+
+    public Credit retrieveCredit(String id) {
+        return (Credit) accountRepository.retrieveAccount(id);
+    }
+
+    public void updateCredit(Credit credit) {
+        accountRepository.updateAccount(credit);
+    }
+
+    public void deleteCredit(String id) {
+        accountRepository.deleteAccount(id);
+    }
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        Credit account = (Credit) accountRepository.retrieveAccount(id);
+        account.setCredit(account.getCredit().add(amount));
+        updateCredit(account);
+    }
+
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        Credit account = (Credit) accountRepository.retrieveAccount(id);
+        account.setCredit(account.getCredit().subtract(amount));
+        updateCredit(account);
+    }
 }
