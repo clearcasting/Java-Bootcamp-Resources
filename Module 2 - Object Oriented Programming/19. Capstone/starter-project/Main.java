f32d62bb07a5636e724fa62e39c4ff1a83251c04
clearcasting <lateralcreativity@outlook.com>
2024-06-17 22:27:09 -0500

    capstone complete we go springboot course

--- Module 2 - Object Oriented Programming/19. Capstone/starter-project/Main.java
+++ Module 2 - Object Oriented Programming/19. Capstone/starter-project/Main.java
@@ -4,39 +4,87 @@ import repository.TradeAccountRepository;
 import service.CashAccountService;
 import service.MarginAccountService;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
+    static Path[] paths = new Path[] {Paths.get("data/accounts.txt"), Paths.get("data/transactions.txt")};
+
+
+    static TradeAccountRepository tradeAccountRepository = new TradeAccountRepository();
+    static CashAccountService cashAccountService = new CashAccountService(tradeAccountRepository);
+    static MarginAccountService marginAccountService = new MarginAccountService(tradeAccountRepository);
+
+
     public static void main(String[] args) {
-        TradeAccountRepository repository = new TradeAccountRepository();
-        CashAccountService cashAccountService = new CashAccountService(repository);
-        MarginAccountService marginAccountService = new MarginAccountService(repository);
 
-        // Create CashAccount and MarginAccount objects
-        CashAccount cashAccount = new CashAccount("1", BigDecimal.valueOf(1000));
-        MarginAccount marginAccount = new MarginAccount("2", BigDecimal.valueOf(2000));
-
-        // Add the accounts to the repository
-        cashAccountService.createTradeAccount(cashAccount);
-        marginAccountService.createTradeAccount(marginAccount);
-
-        // Deposit and withdraw amounts
-        cashAccountService.deposit("1", BigDecimal.valueOf(500));
-        cashAccountService.withdraw("1", BigDecimal.valueOf(200));
-        marginAccountService.deposit("2", BigDecimal.valueOf(1000));
-        marginAccountService.withdraw("2", BigDecimal.valueOf(500));
-
-        // Retrieve and print the updated account balances
-        CashAccount updatedCashAccount = cashAccountService.retrieveTradeAccount("1");
-        MarginAccount updatedMarginAccount = marginAccountService.retrieveTradeAccount("2");
-        System.out.println("Updated CashAccount balance: " + updatedCashAccount.getCashBalance());
-        System.out.println("Updated MarginAccount margin: " + updatedMarginAccount.getMargin());
-
-        // Delete the accounts
-        cashAccountService.deleteTradeAccount("1");
-        marginAccountService.deleteTradeAccount("2");
+        try {
+            loadTradeAccounts();
+            applyTransactions();
+            finalTest();
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
 
 
 
+    }
+
+    public static void finalTest() throws IOException {
+        System.out.println("Account A1234B Cash Balance: " + cashAccountService.retrieveTradeAccount("A1234B").getCashBalance());
+        System.out.println("Account E3456F Cash Balance: " + cashAccountService.retrieveTradeAccount("E3456F").getCashBalance());
+        System.out.println("Account I5678J Cash Balance: " + cashAccountService.retrieveTradeAccount("I5678J").getCashBalance());
+        System.out.println("Account C2345D Margin: " + marginAccountService.retrieveTradeAccount("C2345D").getMargin());
+        System.out.println("Account G4567H Margin: " + marginAccountService.retrieveTradeAccount("G4567H").getMargin());
+    }
+
+
+    public static void loadTradeAccounts() {
+        try {
+            Files.lines(paths[0])
+                    .forEach(account -> {
+                        String[] split = account.split(" ");
+                        if (split[0].equals("CASH")) {
+                            cashAccountService.createTradeAccount(new CashAccount(split[1], new BigDecimal(split[2])));
+                        } else {
+                            marginAccountService.createTradeAccount(new MarginAccount(split[1], new BigDecimal(split[2])));
+                        }
+                    });
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public static void applyTransactions() {
+        try {
+            Files.lines(paths[1])
+                    .forEach(transaction -> {
+                        String[] split = transaction.split(" ");
+                        String id = split[1];
+                        BigDecimal amount = new BigDecimal(split[3]);
+
+                        switch (split[2]) {
+                            case "DEPOSIT" -> {
+                                if (split[0].equals("CASH")) {
+                                    cashAccountService.deposit(id, amount);
+                                } else {
+                                    marginAccountService.deposit(id, amount);
+                                }
+                            }
+                            case "WITHDRAWAL" -> {
+                                if (split[0].equals("CASH")) {
+                                    cashAccountService.withdraw(id, amount);
+                                } else {
+                                    marginAccountService.withdraw(id, amount);
+                                }
+                            }
+                        }
+                    });
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
