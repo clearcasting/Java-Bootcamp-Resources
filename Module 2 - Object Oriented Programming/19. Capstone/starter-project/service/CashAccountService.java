18107d26aad494592d5cda99a04e7be6bf2c6687
clearcasting <lateralcreativity@outlook.com>
2024-06-17 19:20:09 -0500

    part 3 done

--- Module 2 - Object Oriented Programming/19. Capstone/starter-project/service/CashAccountService.java
+++ Module 2 - Object Oriented Programming/19. Capstone/starter-project/service/CashAccountService.java
@@ -1,7 +1,45 @@
 package service;
 
 
-public class CashAccountService {
+import pojo.CashAccount;
+import repository.TradeAccountRepository;
 
+import java.math.BigDecimal;
 
+public class CashAccountService implements TradeAccountService{
+    private TradeAccountRepository repository;
+
+    public CashAccountService(TradeAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    public void createTradeAccount(CashAccount account) {
+        repository.createTradeAccount(account);
+    }
+
+    public CashAccount retrieveTradeAccount(String id) {
+        return (CashAccount) repository.retrieveTradeAccount(id);
+    }
+
+    public void updateTradeAccount(CashAccount account) {
+        repository.updateTradeAccount(account);
+    }
+
+    public void deleteTradeAccount(String id) {
+        repository.deleteTradeAccount(id);
+    }
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        CashAccount account = retrieveTradeAccount(id);
+        account.setCashBalance(account.getCashBalance().add(amount));
+        updateTradeAccount(account);
+    }
+
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        CashAccount account = retrieveTradeAccount(id);
+        account.setCashBalance(account.getCashBalance().subtract(amount));
+        updateTradeAccount(account);
+    }
 }
--- Module 2 - Object Oriented Programming/19. Capstone/starter-project/service/MarginAccountService.java
+++ Module 2 - Object Oriented Programming/19. Capstone/starter-project/service/MarginAccountService.java
@@ -1,7 +1,45 @@
 package service;
 
 
-public class MarginAccountService {
+import pojo.MarginAccount;
+import repository.TradeAccountRepository;
 
+import java.math.BigDecimal;
 
+public class MarginAccountService implements TradeAccountService {
+    private TradeAccountRepository repository;
+
+    public MarginAccountService(TradeAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    public void createTradeAccount(MarginAccount account) {
+        repository.createTradeAccount(account);
+    }
+
+    public MarginAccount retrieveTradeAccount(String id) {
+        return (MarginAccount) repository.retrieveTradeAccount(id);
+    }
+
+    public void updateTradeAccount(MarginAccount account) {
+        repository.updateTradeAccount(account);
+    }
+
+    public void deleteTradeAccount(String id) {
+        repository.deleteTradeAccount(id);
+    }
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        MarginAccount account = retrieveTradeAccount(id);
+        account.setMargin(account.getMargin().add(amount));
+        updateTradeAccount(account);
+    }
+
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        MarginAccount account = retrieveTradeAccount(id);
+        account.setMargin(account.getMargin().subtract(amount));
+        updateTradeAccount(account);
+    }
 }
